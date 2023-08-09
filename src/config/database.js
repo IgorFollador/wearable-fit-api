685422263faa89d94b7a3ac842805a313d33c441
Igor Follador <igorledf@hotmail.com>
2023-08-09 10:05:15 -0300

    Add enviroment variable DB_DIALECT to database.js

--- src/config/database.js
+++ src/config/database.js
@@ -6,20 +6,20 @@ module.exports = {
     "password": process.env.DB_DEV_PASSWORD,
     "database": process.env.DB_DEV_DATABASE,
     "host": process.env.DB_DEV_HOST,
-    "dialect": "mysql"
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": process.env.DB_TEST_USERNAME,
     "password": process.env.DB_TEST_PASSWORD,
     "database": process.env.DB_TEST_DATABASE,
     "host": process.env.DB_TEST_HOST,
-    "dialect": "mysql"
+    "dialect": process.env.DB_DIALECT
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": process.env.DB_DIALECT
   }
 };
