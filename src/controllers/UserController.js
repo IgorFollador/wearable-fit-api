245a2e1edf27f8875a3ff2b592ac3a1be9834aa9
Igor Follador <igorledf@hotmail.com>
2023-08-11 10:21:12 -0300

    Create function and route to recover User after delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -100,6 +100,25 @@ class UserController {
         }
     }
 
+    static async recover(req, res) {
+        try {
+            const { id } = req.params;
+
+            const user = await database.User.findByPk(id, { paranoid: false });
+            if(user === null) return res.status(404).json({ message: 'User not found'})
+
+            await database.User.restore({
+                where: {
+                    id: Number(id)
+                }
+            });
+
+            return res.status(200).json({ message: `User with ID ${id} recovered` });
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+
 }
 
 module.exports = UserController;
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -7,6 +7,8 @@ router.get('/users', UserController.read);
 
 router.put('/users', UserController.update);
 
+router.put('/users/recover/:id', UserController.recover)
+
 router.delete('/users/:id', UserController.delete);
 
 module.exports = router;
